1939e58ee6071887f13c95783c4b2800de364d52
Tomás <tomaspassadore@gmail.com>
2025-12-15 18:56:55 -0300

    Funcionando 'Dar alta de huesped' y 'Estado habitaciones'

--- backend/src/main/java/com/reservas/hotel/api_gestion_hotelera/entities/Habitacion.java
+++ backend/src/main/java/com/reservas/hotel/api_gestion_hotelera/entities/Habitacion.java
@@ -2,28 +2,48 @@ package com.reservas.hotel.api_gestion_hotelera.entities;
 
 import com.reservas.hotel.api_gestion_hotelera.entities.enums.EstadoHabitacion;
 
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.EnumType;
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity; // Para @Entity, @Id, @GeneratedValue
-import jakarta.persistence.GeneratedValue; // Para @Data
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Habitacion {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // Clave primaria autogenerada
+    private Long id;
 
+    @Column(nullable = false, unique = true)
     private String numero;
+    
+    @Column(nullable = false, unique = true)
     private String idHabitacion;
-    @Enumerated(EnumType.ORDINAL)
+    
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private EstadoHabitacion estado;
 
-    // (Relaciones con TipoHabitacion y CostoPorNoche se añadirán luego)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "tipo_habitacion_id", nullable = false)
+    private TipoHabitacion tipoHabitacion;
+    
+    public Habitacion(String numero, String idHabitacion, EstadoHabitacion estado, TipoHabitacion tipoHabitacion) {
+        this.numero = numero;
+        this.idHabitacion = idHabitacion;
+        this.estado = estado;
+        this.tipoHabitacion = tipoHabitacion;
+    }
 }
